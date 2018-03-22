54428e02b633ff9267628b6e049829d707e96b62
peter.storm <peter.storm@peterstorm.io>
2018-03-22 09:50:14 -0400

    add caseExpressions

--- patternMatching.hs
+++ patternMatching.hs
@@ -77,3 +77,30 @@ antarcticPenguin _                 = False
 antarcticOrGalapagos :: Penguin -> Bool
 antarcticOrGalapagos p =
   (galapagosPenguin p) || (antarcticPenguin p)
+
+-- TUBLES
+-- first example not pattern matching
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f x y = ((snd x, snd y), (fst x, fst y))
+
+-- Or written with pattern matching
+f' :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f' (a, b) (c, d) = ((b, d), (a, c))
+
+addEmUp2 :: Num a => (a, a) -> a
+addEmUp2 (x, y) = x + y
+
+addEmUp2Alt :: Num a => (a, a) -> a
+addEmUp2Alt tup = (fst tup) + (snd tup)
+
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+third3 :: (a, b, c) -> c
+third3 (_, _, x) = x
+
+-- EXERCISES
+f'' :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
+f'' (a, b, c) (d, e, f) = ((a, d), (c, f))
+
+
